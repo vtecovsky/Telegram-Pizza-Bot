417d1d225c4cd8f2f9ea09fa7776fb0401650452
Saveliy <s.lekhtin@innopolis.university>
2023-03-03 13:51:42 +0300

    Запуск через файл telegram_bot.py , а не bat-файл

---
+++ create_bot.py
@@ -0,0 +1,9 @@
+from aiogram import Bot
+from aiogram.dispatcher import Dispatcher
+import config
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
+storage = MemoryStorage()
+
+bot = Bot(token=config.TOKEN)
+dp = Dispatcher(bot, storage=storage)
